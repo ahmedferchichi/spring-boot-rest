3305cb816f462ae7de445db4f98a97f98228006a
ahmedferchichi <ferchichy.ahmed@gmail.com>
2020-05-08 02:08:59 +0200

    Correction the import of User

--- src/main/java/com/devglan/dao/UserDao.java
+++ src/main/java/com/devglan/dao/UserDao.java
@@ -1,6 +1,6 @@
 package com.devglan.dao;
 
-import com.devglan.model.User;
+import com.devglan.model.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
